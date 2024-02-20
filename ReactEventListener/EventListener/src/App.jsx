6cc3d5c94f394ca1545b2f5129738ffe0976ce6e
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-02-20 23:12:00 +0530

    updated

--- ReactEventListener/EventListener/src/App.jsx
+++ ReactEventListener/EventListener/src/App.jsx
@@ -31,7 +31,7 @@ export default function App() {
         isOpen && (
           <div className="steps">
             <div className="numbers">
-              <div onMouseEnter={() => alert('1')} className={`${step >= 1 ? 'active' : ''}`}>1</div>
+              <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
               <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
               <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
             </div>
